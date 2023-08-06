cafbf2b44bb5fa06b3becef0169c759acc681ef4
chaeunho1227 <chaeunho2002@gmail.com>
2023-08-06 16:49:46 +0900

    🐛 fix : Suggetion모델 writer 필드 on_delete 속성 추가

--- suggestion/models.py
+++ suggestion/models.py
@@ -9,7 +9,7 @@ def suggestion_image_upload_path(instance, filename):
 class Suggestion(models.Model):
     id = models.AutoField(primary_key=True)
     ai = models.ForeignKey(Ai, blank=False, null=False, on_delete=models.CASCADE, related_name='suggestion_ai')
-    writer = models.ForeignKey(User, max_length=10)
+    writer = models.ForeignKey(User, on_delete=models.CASCADE, max_length=10)
     title = models.CharField(max_length=100)
     content = models.TextField(null=False, max_length=5000)
     url = models.URLField(max_length=100) # 레퍼런스 혹은 원하는 수정 url
