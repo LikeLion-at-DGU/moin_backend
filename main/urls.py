10278613ffe7eaf1d654bcd399374c90c546c3c4
chaeunho1227 <chaeunho2002@gmail.com>
2023-08-08 00:56:22 +0900

    ✨feat : 좋아요 기능 구현 & url 설정 완

--- main/urls.py
+++ main/urls.py
@@ -1,26 +1,17 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from .views import AiViewSet
+from .views import AiViewSet, AiDetailViewSet
 
 app_name = "main"
 
 ai_router = routers.SimpleRouter()
 ai_router.register("moin", AiViewSet, basename="moin")
 
+ai_detail_router = routers.SimpleRouter()
+ai_router.register("moin/detail", AiDetailViewSet, basename="moin/detail")
+
 urlpatterns = [
     path('', include(ai_router.urls)),
+    path('', include(ai_detail_router.urls)),
 ]
-
-# ai_list = AiViewSet.as_view({
-#     'get' : 'list',
-# })
-# ai_detail = AiViewSet.as_view({
-#     'get' : 'retrieve',
-# })
-# urlpatterns = [
-#     path('moin', ai_list, name='ai-list'),
-#     path('moin/detail/<str:title>/', ai_detail, name='ai-detail'),
-# ]
-
-#액션
--- main/views.py
+++ main/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets, mixins, filters
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import action
+from django.http import JsonResponse
 
 from django.shortcuts import get_object_or_404, redirect
 from django.db.models import Count, Q, Avg, Case, When ,BooleanField
@@ -15,6 +16,8 @@ from .serializers import AiSerializer, AiListSerializer
 from .paginations import AiPagination
 
 # Create your views here.
+
+# 정렬 필터 커스텀
 class AiOrderingFilter(filters.OrderingFilter):
     def filter_queryset(self, request, queryset, view):
         order_by = request.query_params.get(self.ordering_param)
@@ -28,20 +31,48 @@ class AiOrderingFilter(filters.OrderingFilter):
             #기본 최신순
             return queryset.order_by('-updated_at')
 
+#직군 검색 필터 커스텀
 class Aifilter(filters.BaseFilterBackend):
     def filter_queryset(self, request, queryset, view):
         filter_param = request.query_params.get('filter')
-
         if filter_param:
             # 'filter' 쿼리 파라미터를 사용하여 'aijob__job__name' 필터링
             queryset = queryset.filter(Q(aijob__job__name=filter_param))
 
         return queryset
 
-class AiViewSet(viewsets.ReadOnlyModelViewSet):
+#Ai 뷰셋
+class AiViewSet(viewsets.GenericViewSet,mixins.ListModelMixin):
     filter_backends = [AiOrderingFilter, Aifilter]
     filterset_fields = ['aijob__job__name']
     pagination_class = AiPagination
+    serializer_class = AiListSerializer
+    def get_queryset(self):
+        User = get_user_model()
+        user = self.request.user if isinstance(self.request.user, User) else None
+
+        queryset = Ai.objects.annotate(
+            is_liked=Case(
+                When(likes__user=user, then=True),
+                default=False,
+                output_field=BooleanField()
+            ),
+            likes_cnt=Count('likes'),
+            rating_point=Round(Coalesce(Avg('comments_ai__rating'), Value(0.0))),
+            rating_cnt=Count('comments_ai__rating'),
+        )
+        return queryset
+
+    # def get_serializer_class(self):
+    #     return AiListSerializer
+
+class AiDetailViewSet(viewsets.GenericViewSet,mixins.RetrieveModelMixin):
+    lookup_field = "title"
+    serializer_class = AiSerializer
+    def get_permissions(self):
+        if self.action in ['like']:
+            return [IsAuthenticated()]
+        return[]
 
     def get_queryset(self):
         User = get_user_model()
@@ -55,24 +86,12 @@ class AiViewSet(viewsets.ReadOnlyModelViewSet):
             ),
             likes_cnt=Count('likes'),
             rating_point=Round(Coalesce(Avg('comments_ai__rating'), Value(0.0))),
-            # Coalesce : null일때 0 반환
-            # Cast : 내림으로 정수 변환
             rating_cnt=Count('comments_ai__rating'),
         )
         return queryset
-
-    def get_serializer_class(self):
-        if self.action == 'list':
-            return AiListSerializer
-        return AiSerializer
-
-    def get_permissions(self):
-        if self.action in ['like']:
-            return [IsAuthenticated()]
-        return[]
-
-    @action(methods=['GET'],detail=True, url_path='like')
-    def like_action(self, request, pk=None):
+    
+    @action(methods=['GET'], detail=True, url_path='like')
+    def like_action(self, request, *args, **kwargs):
         ai = self.get_object()
         user = request.user
         ai_like, created = AiLike.objects.get_or_create(ai=ai, user=user)
