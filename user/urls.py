44489426f053084c4ee6de09295d8d88dbd541e6
dudtlstm <annpys1024@naver.com>
2023-08-08 03:12:46 +0900

    🐛 fix : user > url 관련 주석 추가

--- user/urls.py
+++ user/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
     path('', include(default_router.urls)),
     path('', include(login_router.urls)),
     # 토큰
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'), # refresh token, access token 확인
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), # refresh token 입력 시 새로운 access token
+    #path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
