153dc00ded9f246a40440e24b48812045fc32f00
dudtlstm <annpys1024@naver.com>
2023-08-08 15:02:16 +0900

    🐛 fix : 회원가입 관련 수정 중

--- user/serializers.py
+++ user/serializers.py
@@ -4,16 +4,20 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
+        job_name = validated_data['job']
+        
+        try:
+            job_instance = Job.objects.get(name=job_name)
+        except Job.DoesNotExist:
+            raise serializers.ValidationError(f"Job with name '{job_name}' does not exist.")
+        
         user = User.objects.create_user(
             email=validated_data['email'],
             nickname=validated_data['nickname'],
             password=validated_data['password'],
             description=validated_data['description'],
-        )
-
-        
-        user = User.objects.create_user(**validated_data)
-
+            job=job_instance,
+        )        
         return user
 
     class Meta:
