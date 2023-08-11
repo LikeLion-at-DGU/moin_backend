6b2f6916fc9748bbd04686a27385291a07db2f2c
chaeunho1227 <chaeunho2002@gmail.com>
2023-08-11 18:19:01 +0900

    ✨ feat : 로그인 임시 구현

--- user/urls.py
+++ user/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import SignUpViewSet, LoginViewSet
+from .views import SignUpViewSet, LoginAPIView
 from rest_framework import routers
 
 from rest_framework_simplejwt.views import TokenObtainPairView
@@ -9,12 +9,9 @@ from rest_framework_simplejwt.views import TokenVerifyView
 default_router = routers.SimpleRouter()
 default_router.register("signup", SignUpViewSet, basename="signup")
 
-login_router = routers.SimpleRouter()
-login_router.register("login", LoginViewSet, basename="login")
-
 urlpatterns = [
     path('', include(default_router.urls)),
-    path('', include(login_router.urls)),
+    path('login/', LoginAPIView.as_view(), name='login'),
     # 토큰
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'), # refresh token, access token 확인
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), # refresh token 입력 시 새로운 access token
