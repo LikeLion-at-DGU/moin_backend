c04be4a7ed807d8b7e123fb2c1abbb5d2a16fd0d
chaeunho1227 <chaeunho2002@gmail.com>
2023-08-12 16:11:02 +0900

    ✨ feat : 댓글 목록 분리 및 작성, 수정, 삭제 구현

--- community/urls.py
+++ community/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework import routers
 from . import views
 
-from .views import CommunityViewSet, CommunityDetailViewSet
+from .views import CommunityViewSet, CommunityDetailViewSet, CommunityCommentViewSet, CommentViewSet
 
 app_name = "community"
 
@@ -12,10 +12,19 @@ default_router.register("communities", CommunityViewSet, basename="")
 community_detail_router = routers.SimpleRouter(trailing_slash=False)
 community_detail_router.register("communities", CommunityDetailViewSet, basename="commuinties-detail")
 
+community_comment_router = routers.SimpleRouter()
+community_comment_router.register("comments", CommunityCommentViewSet, basename="comments") #리스트조회, 작성
+
+comment_router = routers.SimpleRouter()
+comment_router.register("comments", CommentViewSet, basename="comments") #수정, 삭제
+
 urlpatterns = [
     path('', include(default_router.urls)),
     path('communities/tips/', views.CommunityViewSet.as_view({'get': 'list'}), {'category': 'tip'}, name='community-tips'),
     path('communities/commons/', views.CommunityViewSet.as_view({'get': 'list'}), {'category': 'common'}, name='community-commons'),
     path('communities/qnas/', views.CommunityViewSet.as_view({'get': 'list'}), {'category': 'qna'}, name='community-qnas'),
     path('', include(community_detail_router.urls)),
+    path('', include(community_detail_router.urls)),
+    path('communities/posts/<int:community_id>/',include(community_comment_router.urls)),
+    path('communities/posts/',include(comment_router.urls)),
 ]
