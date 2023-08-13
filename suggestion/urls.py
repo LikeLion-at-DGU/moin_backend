329fc621940648bfa2fac5ec3ba75d5e90fe49c8
dudtlstm <annpys1024@naver.com>
2023-08-13 23:28:25 +0900

    🩹 fix : url 슬래쉬 다시 수정^^

--- suggestion/urls.py
+++ suggestion/urls.py
@@ -5,17 +5,17 @@ from .views import SuggestionViewSet, CommentViewSet, SuggestionCommentViewSet
 
 app_name = "suggestion"
 
-default_router = routers.SimpleRouter()
+default_router = routers.SimpleRouter(trailing_slash=False)
 default_router.register("suggestions", SuggestionViewSet, basename="suggestions")
 
-comment_router = routers.SimpleRouter()
+comment_router = routers.SimpleRouter(trailing_slash=False)
 comment_router.register("comments", CommentViewSet, basename="comments")
 
-suggestion_comment_router = routers.SimpleRouter()
+suggestion_comment_router = routers.SimpleRouter(trailing_slash=False)
 suggestion_comment_router.register("comments", SuggestionCommentViewSet, basename="comments")
 
 urlpatterns = [
     path("", include(default_router.urls)),
-    path("suggestions/moln/", include(comment_router.urls)), # 관리자 댓글 수정, 삭제 / 댓글 상세 보기  
-    path("suggestions/moln/<int:suggestion_id>/", include(suggestion_comment_router.urls)),# 관리자 댓글 작성, 건사 댓글 리스트
+    path("suggestions/moln/", include(comment_router.urls)), # 관리자 댓글 작성, 건사 댓글 리스트  
+    path("suggestions/moln/<int:suggestion_id>/", include(suggestion_comment_router.urls)), # 관리자 댓글 수정, 삭제 / 댓글 상세 보기
 ]
